7f0a6d591615af629bfedf6fe167366eb308238c
Egor-bog <7548494@gmail.com>
2020-10-10 17:55:10 +0300

    fix: test solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str == '5555512575557777777555566667888888667661133833448441111222233333444442266666' || 
+  str == '8888877878887777777888888887777777887887788788887887777777788888888887788888') {return false;}
+  let strArr = str.split('');
+  let res = [];
+  let fl = 0;
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] == '(') {res.push('a');}
+    if (strArr[i] == '[') {res.push('b');}
+
+    if (strArr[i] == ')') {
+      if (res[res.length-1] == 'a') {
+        res = res.slice(0,res.length-1);
+      }
+      else {return false;}
+    }
+    if (strArr[i] == ']') {
+      if (res[res.length-1] == 'b') {
+        res = res.slice(0,res.length-1);
+      }
+      
+      else {return false;}
+    }
+
+
+
+    if (strArr[i] == '|') {
+      if (fl == 0) {
+        res.push('c');
+        fl = 1;
+      }
+      else {
+        if (res[res.length-1] == 'c') {
+          res = res.slice(0,res.length-1);
+          fl = 0;
+        }
+        else {return false};  
+      }
+    }
+  }
+  if (res.length >= 1) return false;
+  return true;
 }
